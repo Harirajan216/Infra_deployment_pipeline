eecd32165cb9fd3ac0e942709863cb47e39ec6b6
Harirajan216 <harirajan@gmail.com>
2026-01-02 13:59:56 +0530

    final commit v2

--- env/Dev/main.tf
+++ env/Dev/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
 module "ec2" {
   source            = "../../modules/ec2"
   ami_id            = "ami-0e7938ad51d883574"
-  instance_type     = "t2.micro"
+  instance_type     = "t3.micro"
   subnet_id         = module.vpc.subnet_id
   ec2_count         = 2
   env               = "dev"
