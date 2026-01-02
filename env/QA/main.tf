c264be263c4b75d11bb2817ef5158831611a5c4d
Harirajan216 <harirajan@gmail.com>
2026-01-02 15:47:38 +0530

    Update QA environment configs

---
+++ env/QA/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source        = "../../modules/vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  subnet_cidr   = "10.0.1.0/24"
+  subnet_az     = "${var.aws_region}a"          # ap-southeast-1a
+  env           = "QA"
+}
+
+module "ec2" {
+  source            = "../../modules/ec2"
+  ami_id            = "ami-0e7938ad51d883574"
+  instance_type     = "t3.micro"
+  subnet_id         = module.vpc.subnet_id
+  ec2_count         = 2
+  env               = "QA"
+}
