b176b4b7335609d02c6ff80e548418d177244739
Harirajan216 <harirajan@gmail.com>
2026-01-02 17:19:47 +0530

    Deploying code to prod

---
+++ env/Prod/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraformbucket2026"
+    key            = "env/Prod/terraform.tfstate"
+    region         = "ap-south-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source        = "../../modules/vpc"
-  vpc_cidr      = "11.0.0.0/16"
-  subnet_cidr   = "11.0.1.0/24"
+  vpc_cidr      = "12.0.0.0/16"
+  subnet_cidr   = "12.0.1.0/24"
   subnet_az     = "${var.aws_region}a"          # ap-southeast-1a
-  env           = "QA"
+  env           = "Prod"
 }
 
 module "ec2" {
@@ -12,5 +12,5 @@ module "ec2" {
   instance_type     = "t3.micro"
   subnet_id         = module.vpc.subnet_id
   ec2_count         = 2
-  env               = "QA"
+  env               = "Prod"
 }
