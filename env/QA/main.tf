84b6ca0b2a280119fc07bf32b92d1c6dbf68e558
Harirajan216 <harirajan@gmail.com>
2026-01-02 16:46:58 +0530

    deploying QA code v2

--- env/QA/main.tf
+++ env/QA/main.tf
@@ -6,13 +6,11 @@ module "vpc" {
   env           = "QA"
 }
 
-
 module "ec2" {
   source            = "../../modules/ec2"
   ami_id            = "ami-0e7938ad51d883574"
   instance_type     = "t3.micro"
   subnet_id         = module.vpc.subnet_id
-  ec2_count         = 3
+  ec2_count         = 0
   env               = "QA"
 }
-
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "myec2" {
-  ami           = ami_id
-  instance_type = instance_type
-  subnet_id     = subnet_id
-  count         = ec2_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  count         = var.ec2_count
 
   tags = {
-    Name  = "${var.var.env}--ec2-${count.index}"
-    env   = var.env
+    Name = "${var.env}-ec2-${count.index}"
+    Env  = var.env
   }
 }
